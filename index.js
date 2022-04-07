c29e5958a44f3a19d5df3da27d5b9e3a7e5ea753
Karthik <56579449+karthik9728@users.noreply.github.com>
2022-04-07 15:16:29 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let name ="Console Message Application Started";
+let person ={
+    name :"XXX",
+    age : 30
+}
+let color = ["Red","Green",1];
+
+function Car(model){
+    carName="audi "+ model;
+    console.log(carName);
+}
+
+Car("A6");
+
+console.log(color);
+console.log(typeof(color[1]));
+console.log(person);
+console.log(person.name);
+console.log(person.age);
